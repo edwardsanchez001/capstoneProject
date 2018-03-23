85b138495839c34312c35cf822b852d7a91fa556
edwardsanchez001 <jesus.ed.sanchez@gmail.com>
2018-03-23 13:35:40 -0500

    have a basic css layout for materials and colors along with the search functionality working for both.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -274,4 +274,10 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  #
+  #
+  config.sign_out_via = :get
+
+  config.sign_out_via = Rails.env.test? ? :get : :delete
+  #
 end
--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,9 @@ Rails.application.routes.draw do
 
   get 'pages/materialPage'
 
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
